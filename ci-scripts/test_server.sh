881db4d542d9ee0474ae4df3ed9aca182ede9b12
Or David <ordavidil@gmail.com>
2017-02-12 20:18:22 +0200

    Make sure BUILD_WEBDRIVERIO is being set before running the docker.

--- ci-scripts/test_server.sh
+++ ci-scripts/test_server.sh
@@ -12,4 +12,9 @@ if [ -z ${BUILD_SERVER+x} ] || [ "$BUILD_SERVER" -ne 1 ]; then
  exit 0;
 fi
 
+# Set 'BUILD_WEBDRIVERIO' in case it is not defined.
+if [ -z ${BUILD_WEBDRIVERIO+x} ]; then
+ BUILD_WEBDRIVERIO=0
+fi
+
 docker run -it -p 8080:80 server -e BUILD_WEBDRIVERIO=$BUILD_WEBDRIVERIO
