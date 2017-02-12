336dcba7848fa1e1dbf20f8b3a6d827d73741a6c
Or David <ordavidil@gmail.com>
2017-02-12 19:18:09 +0200

    Echo standard between quotes ...

--- ci-scripts/test_coder.sh
+++ ci-scripts/test_coder.sh
@@ -35,7 +35,7 @@ code_review () {
 IGNORED_PATTERNS="*.features.inc,*.features.*.inc,*.field_group.inc,*.strongarm.inc,*.ds.inc,*.context.inc,*.pages.inc,*.pages_default.inc,*.views_default.inc,*.file_default_displays.inc,*.facetapi_defaults.inc,*.panels_default.inc"
 
 echo
-echo "${LBLUE}> Sniffing Modules following $REVIEW_STANDARD standard. ${RESTORE}"
+echo "${LBLUE}> Sniffing Modules following '${REVIEW_STANDARD}' standard. ${RESTORE}"
 
 for dir in $TRAVIS_BUILD_DIR/server/hedley/modules/custom/*/ ; do
   code_review $dir $IGNORED_PATTERNS
