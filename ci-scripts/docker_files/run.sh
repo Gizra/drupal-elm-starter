26979b5eaf695c81170fe07f992d7fb2fd3de0fc
Aron Novak <aron@novaak.net>
2017-02-28 11:19:46 +0100

    ci-scripts: added the ability to debug Docker container interactively

--- ci-scripts/docker_files/run.sh
+++ ci-scripts/docker_files/run.sh
@@ -3,8 +3,23 @@
 # Load helper functionality.
 cd ci-scripts/docker_files
 
-bash preparing_server.sh
-bash preparing_client.sh
 
-bash run_behat.sh
-bash run_webdriverio.sh
+if [ -v $DOCKER_DEBUG ]; then
+  # Regular Travis execution, failing on the first error is what we want.
+  bash preparing_server.sh
+  bash preparing_client.sh
+
+  bash run_behat.sh
+  bash run_webdriverio.sh
+
+else
+  # We keep going and open a Bash shell to interactively inspect what failed or
+  # what's the status of the application.
+  bash preparing_server.sh || :
+  bash preparing_client.sh || :
+
+  bash run_behat.sh || :
+  bash run_webdriverio.sh || :
+
+  bash
+fi
