cdd40d26cb9db8c71111c009f637398f69814e80
Aron Novak <aron@novaak.net>
2017-03-02 16:44:19 +0100

    initial idea for making the things working under Travis, installing docker-compose, and replacing "docker run" by docker-compose up at the right place

--- ci-scripts/test_server.sh
+++ ci-scripts/test_server.sh
@@ -17,4 +17,5 @@ if [ -z ${BUILD_WEBDRIVERIO+x} ]; then
  BUILD_WEBDRIVERIO=0
 fi
 
-docker run -it -p 8080:80 -e BUILD_WEBDRIVERIO=$BUILD_WEBDRIVERIO server
+cd ci-scripts/docker_files
+docker-compose up
