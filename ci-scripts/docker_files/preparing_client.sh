0dbdc70d14b754c7d70aafd495090c967b00cc60
Aron Novak <aron@novaak.net>
2017-03-02 09:44:09 +0100

    preparing_client.sh: adapting to https://github.com/Gizra/drupal-elm-starter/commit/2eba1401669bc6a7bd01bac147ca6269c6b16507, supply local config [skip ci]

--- ci-scripts/docker_files/preparing_client.sh
+++ ci-scripts/docker_files/preparing_client.sh
@@ -25,6 +25,7 @@ npm install
 bower install --allow-root
 
 elm-package install -y
+cp src/elm/LocalConfig.Example.elm src/elm/LocalConfig.elm
 
 # Getting elm-make to run quicker.
 # See https://github.com/elm-lang/elm-compiler/issues/1473#issuecomment-245704142
