d7cc59edd58a9152df146b38238b85819d961e51
Aron Novak <aron@novaak.net>
2017-03-07 09:07:19 +0100

    wdio.conf.js: raise timeout to adapt to slower Travis environment

--- client/wdio.conf.js
+++ client/wdio.conf.js
@@ -75,7 +75,7 @@ exports.config = {
     baseUrl: 'http://server.local:3000',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout: 20000,
     //
     // Default timeout in milliseconds for request
     // if Selenium Grid doesn't send response
