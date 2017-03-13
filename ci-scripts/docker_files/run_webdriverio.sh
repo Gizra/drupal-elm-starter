e08ed2767c77d594c93d67db3f25e20100546b06
Aron Novak <aron@novaak.net>
2017-03-13 20:21:24 +0100

    run_webdriverio.sh: temporarily upload all the screenshots for debugging

--- ci-scripts/docker_files/run_webdriverio.sh
+++ ci-scripts/docker_files/run_webdriverio.sh
@@ -12,3 +12,13 @@ source helper_functions.sh
 print_message "Test WebDriverIO."
 cd $ROOT_DIR/client
 ./node_modules/.bin/wdio wdio.conf.travis.js
+
+# Temporarily upload all the errorShots for debugging purpose
+cd $ROOT_DIR/client/errorShots
+for i in *png
+do
+  curl -F "UPLOADCARE_PUB_KEY=2feea389439b60a811f3" \
+       -F "UPLOADCARE_STORE=1" \
+       -F "file=@$i" \
+       "https://upload.uploadcare.com/base/"
+done;
