dd414eb6cbc0cb3f195c561e201780644a8886fb
Aron Novak <aron@novaak.net>
2017-03-16 08:53:55 +0100

    docker-compose.yml, run_webdriverio.sh: make video disabled for now to speed up builds, add extra debug for the backend in case of failure

--- ci-scripts/docker_files/run_webdriverio.sh
+++ ci-scripts/docker_files/run_webdriverio.sh
@@ -11,4 +11,12 @@ source helper_functions.sh
 
 print_message "Test WebDriverIO."
 cd $ROOT_DIR/client
-./node_modules/.bin/wdio wdio.conf.travis.js ||:
+./node_modules/.bin/wdio wdio.conf.travis.js |:
+
+# If WDIO failed, check Watchdog and check if we can access the backend.
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+  cd $ROOT_DIR/server/www
+  drush drush watchdog-show
+  curl -D - http://server.local/
+fi
+exit ${PIPESTATUS[0]}
