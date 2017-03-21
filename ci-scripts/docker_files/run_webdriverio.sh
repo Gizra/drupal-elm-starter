d1d2a6c9949d0e0631e7eab05a3dda5d3d67ef0b
Aron Novak <aron@gizra.com>
2017-03-21 14:23:24 +0100

    run_webdriverio.sh, client: port recent developments on the WDIO test execution

--- ci-scripts/docker_files/run_webdriverio.sh
+++ ci-scripts/docker_files/run_webdriverio.sh
@@ -11,16 +11,37 @@ source helper_functions.sh
 
 print_message "Test WebDriverIO."
 cd $ROOT_DIR/client
+
+WDIO_ALL_RET=0
 set +o errexit
-./node_modules/.bin/wdio wdio.conf.travis.js
-WDIO_RET=$?
+for SPEC in test/specs/*js; do
+  echo "---------"
+  echo "Executing $SPEC"
+  echo "---------"
+  echo ""
+  WDIO_RET=0
+  for i in `seq 3`; do
+    ./node_modules/.bin/wdio wdio.conf.travis.js --spec $SPEC
+    WDIO_RET=$?
+    if [[ $WDIO_RET -eq 0 ]]; then
+      # We give 3 chances to complete
+      # but of course we quit on first success
+      break
+    fi
+    echo "$SPEC failed for the attempt ($i.), retrying..."
+  done
+  if [[ $WDIO_RET -ne 0 ]]; then
+    echo "$SPEC failed"
+    WDIO_ALL_RET=$WDIO_RET
+  fi
+done
 
 # If WDIO failed, check Watchdog and check if we can access the backend.
-if [[ $WDIO_RET -ne 0 ]]; then
+if [[ $WDIO_ALL_RET -ne 0 ]]; then
   cd $ROOT_DIR/server/www
   export PATH="$HOME/.composer/vendor/bin:$PATH"
   drush cc drush
   drush watchdog-show
   curl -D - http://server.local/
 fi
-exit $WDIO_RET
+exit $WDIO_ALL_RET
