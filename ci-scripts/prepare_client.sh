752d4799e31341a2e9e4b2de997786e15581a85c
Aron Novak <aron@gizra.com>
2017-04-14 11:08:24 +0200

    ci-scripts/prepare_client.sh: fix test compilation

--- ci-scripts/prepare_client.sh
+++ ci-scripts/prepare_client.sh
@@ -12,5 +12,5 @@ if [ -z ${BUILD_CLIENT+x} ] || [ "$BUILD_CLIENT" -ne 1 ]; then
  exit 0;
 fi
 
-$TRAVIS_BUILD_DIR/sysconfcpus/bin/sysconfcpus -n 2 elm-make $TRAVIS_BUILD_DIR/client/src/elm/TestRunner.elm --output test.html --yes
+$TRAVIS_BUILD_DIR/sysconfcpus/bin/sysconfcpus -n 2 elm-make $TRAVIS_BUILD_DIR/client/src/elm/TestRunner.elm $TRAVIS_BUILD_DIR/client/src/elm/App/Test.elm --output test.html --yes
 cp ./client/src/elm/LocalConfig.Example.elm ./client/src/elm/LocalConfig.elm
