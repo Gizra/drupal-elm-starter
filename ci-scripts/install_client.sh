26b573fb367e2bb432f5b1ccb8673838aa97dfeb
Aron Novak <aron@gizra.com>
2017-05-25 10:17:47 +0200

    restore global elm-test, but with version spec

--- ci-scripts/install_client.sh
+++ ci-scripts/install_client.sh
@@ -16,6 +16,7 @@ echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
 # Install global packages.
 npm install -g elm@~0.18.0
+npm install -g elm-test@0.18.2
 
 cd "$TRAVIS_BUILD_DIR"/client
 elm-package install -y
