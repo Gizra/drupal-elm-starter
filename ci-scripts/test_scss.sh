a47275d9234624af883fb6a35a62c0050a72a770
Aron Novak <aron@gizra.com>
2017-06-30 13:38:13 +0200

    fix shellcheck error, add scss to the invoked scripts in travis.yml [skip ci]

--- ci-scripts/test_scss.sh
+++ ci-scripts/test_scss.sh
@@ -11,11 +11,11 @@ if [ -z "${SCSS_REVIEW+x}" ] || [ "$SCSS_REVIEW" -ne 1 ]; then
  exit 0;
 fi
 
-source "$TRAVIS_BUILD_DIR"/server/travis.config.sh
+"$TRAVIS_BUILD_DIR"/server/travis.config.sh
 
 HAS_ERRORS=0
 
-SCRIPTS=$(find ci-scripts server/$PROFILE_NAME/themes/custom -name '*.scss')
+SCRIPTS=$(find ci-scripts server/"$PROFILE_NAME"/themes/custom -name '*.scss')
 for FILE in $SCRIPTS;  do
   if ! csscss "$FILE"; then
     HAS_ERRORS=1
