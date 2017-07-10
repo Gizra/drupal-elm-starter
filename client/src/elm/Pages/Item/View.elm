14507ffc9a1285dba10b1e1cf76cc575be9d903d
Itamar Shapira Bar-Lev <bitamar@gmail.com>
2017-07-10 11:35:27 +0200

    showMaybe privateNote [skip ci]

--- client/src/elm/Pages/Item/View.elm
+++ client/src/elm/Pages/Item/View.elm
@@ -6,27 +6,34 @@ import Html.Attributes exposing (..)
 import Pages.Item.Model exposing (Msg(..))
 import Item.Model exposing (ItemId, Item)
 import User.Model exposing (User)
-import Utils.Html exposing (divider)
+import Utils.Html exposing (divider, showMaybe)
 
 
 view : Date -> User -> ItemId -> Item -> Html Msg
 view currentDate currentUser itemId item =
-    div []
-        [ div
-            [ class "ui secondary pointing fluid menu" ]
-            [ h2
-                [ class "ui header" ]
-                [ text item.name ]
-            , div
-                [ class "right menu" ]
-                [ a
-                    [ class "ui active item" ]
-                    [ text "Overview" ]
+    let
+        privateNote =
+            showMaybe <|
+                Maybe.map
+                    (\note -> div [ class "private-note" ] [ text note ])
+                    item.privateNote
+    in
+        div []
+            [ div
+                [ class "ui secondary pointing fluid menu" ]
+                [ h2
+                    [ class "ui header" ]
+                    [ text item.name ]
+                , div
+                    [ class "right menu" ]
+                    [ a
+                        [ class "ui active item" ]
+                        [ text "Overview" ]
+                    ]
                 ]
+            , div []
+                [ img [ src item.image, alt item.name ] []
+                ]
+            , divider
+            , privateNote
             ]
-        , div []
-            [ img [ src item.image, alt item.name ] []
-            ]
-        , divider
-        , div [] [ text <| Maybe.withDefault "" item.privateNote ]
-        ]
