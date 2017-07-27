a8a6fd728190a987bbbad07bf15abf1ec74c5b25
Aron Novak <aron@gizra.com>
2017-07-27 12:23:16 +0200

    Revert "new dummy test"
    This reverts commit 344d182e2146139387e79b5e2290becbac2068c6.

--- client/test/specs/zoo.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-
-describe('home', function() {
-    it('should allow a user to login', function() {
-        browser.url('/');
-
-        browser.waitForVisible('.login-form');
-    });
-});
