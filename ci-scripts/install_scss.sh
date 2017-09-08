83a80a19c9d48980d388d21d57970c1aafabb40f
Aron Novak <aron@gizra.com>
2017-09-08 22:29:11 +0200

    ci-scripts/install_scss.sh: fix typo in lint coding standard clone destination path

--- ci-scripts/install_scss.sh
+++ ci-scripts/install_scss.sh
@@ -15,7 +15,7 @@ fi
 npm install -g stylelint@7.12.0
 gem install -v 3.4.24 sass
 gem install -v 1.3.3 csscss
-git clone https://github.com/stylelint/stylelint-config-standard.git .stylelint-config-standar
+git clone https://github.com/stylelint/stylelint-config-standard.git .stylelint-config-standard
 
 # stylelint is sensitive to have the full path for the base ruleset.
 sed -i "s|BASEDIR|$TRAVIS_BUILD_DIR|g" .stylelintrc.json.example
