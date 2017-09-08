019430b4174951885b7cef5ce4545535fc613344
Aron Novak <aron@gizra.com>
2017-09-08 22:57:11 +0200

    ci-scripts/test_scss.sh: adapted for local execution [skip ci]

--- ci-scripts/test_scss.sh
+++ ci-scripts/test_scss.sh
@@ -7,15 +7,16 @@
 # ---------------------------------------------------------------------------- #
 
 # Check the current build.
-if [[ ( -z "${SCSS_REVIEW+x}" || "$SCSS_REVIEW" -ne 1 ) && "$CI" -eq "TRUE" ]]; then
- exit 0;
-fi
-
-if [[ "$CI" -eq "TRUE" ]]; then
-  source "$TRAVIS_BUILD_DIR"/server/travis.config.sh
-else
+if [[ -z ${CI+x} ]]; then
+  echo "Local environment is detected"
   DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   source "$DIR"../server/travis.config.sh
+
+else
+  if [[ -z "${SCSS_REVIEW+x}" || "$SCSS_REVIEW" -ne 1 ]]; then
+    exit 0;
+  fi
+  source "$TRAVIS_BUILD_DIR"/server/travis.config.sh
 fi
 
 HAS_ERRORS=0
