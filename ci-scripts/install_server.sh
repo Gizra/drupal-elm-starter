02b2770991dc86c54996777abeb11087d02ed057
Aron Novak <aron@gizra.com>
2017-09-18 16:35:35 +0200

    ci-scripts/install_server.sh: the real steps to prepare server, not yet actualized to Trusty image

--- ci-scripts/install_server.sh
+++ ci-scripts/install_server.sh
@@ -1,24 +1,48 @@
-#!/bin/sh
-set -e
+#!/usr/bin/env bash
 
-# ---------------------------------------------------------------------------- #
-#
-# Install server dependencies.
-#
-# ---------------------------------------------------------------------------- #
+# Load helper functionality.
+source helper_functions.sh
 
-# Check the current build.
-if [ -z "${BUILD_SERVER+x}" ] || [ "$BUILD_SERVER" -ne 1 ]; then
- exit 0;
+# -------------------------------------------------- #
+# Start MySQL.
+# -------------------------------------------------- #
+print_message "Start MySQL."
+service mysql start
+check_last_command
+
+# -------------------------------------------------- #
+# Configure apache2.
+# -------------------------------------------------- #
+print_message "Configure apache2."
+cd "$ROOT_DIR" || exit 1
+cp ci-scripts/docker_files/default.apache2.conf /etc/apache2/apache2.conf
+service apache2 restart
+cp ci-scripts/docker_files/server.conf /etc/apache2/sites-available/
+a2ensite server.conf
+service apache2 reload
+echo "127.0.0.1 server.local" >> /etc/hosts
+check_last_command
+
+# -------------------------------------------------- #
+# Installing Drush.
+# -------------------------------------------------- #
+print_message "Install drush."
+export PATH="$HOME/.composer/vendor/bin:$PATH"
+# Check drush version.
+drush --version
+cd "$ROOT_DIR" || exit 1
+cp ci-scripts/docker_files/aliases.drushrc.php ~/.drush/
+check_last_command
+
+# -------------------------------------------------- #
+# Installing Profile.
+# -------------------------------------------------- #
+print_message "Install Drupal."
+cd "$ROOT_DIR"/server || exit 1
+cp travis.config.sh config.sh
+if [[ -d www ]]; then
+  bash scripts/reset -dy
+else
+./install -dy
 fi
-
-# Build our own Docker image based on https://github.com/Gizra/drupal-lamp.
-docker build -t server -f "$TRAVIS_BUILD_DIR"/ci-scripts/docker_files/Dockerfile .
-
-# Simple Docker run, no need for Zalenium dependencies.
-if [ -z "${BUILD_WEBDRIVERIO+x}" ]; then
-  exit 0;
-fi
-
-# Zalenium requires to download this dependency image first.
-docker pull elgalu/selenium
+check_last_command
