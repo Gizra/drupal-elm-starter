087207379d3be8a70e73b8270687212f8250d831
Aron Novak <aron@gizra.com>
2017-10-03 17:00:17 +0200

    ci-scripts/docker_files/run_simpletest.sh: fixed shellcheck [skip ci]

--- ci-scripts/docker_files/run_simpletest.sh
+++ ci-scripts/docker_files/run_simpletest.sh
@@ -2,7 +2,7 @@
 set -e
 
 # We should not run the current test under the WebDriverIO build.
-if [ ${BUILD_WEBDRIVERIO} == 1 ]; then
+if [ "${BUILD_WEBDRIVERIO}" == 1 ]; then
  exit 0;
 fi
 
@@ -15,8 +15,8 @@ source helper_functions.sh
 print_message "Run SimpleTest."
 export PATH="$HOME/.composer/vendor/bin:$PATH"
 drush @server.local en simpletest -y
-cd $ROOT_DIR/server
-php ./www/scripts/run-tests.sh --cache --php $(which php) --concurrency 4 --verbose --color --url http://server.local Hedley 2>&1 | tee /tmp/simpletest-result.txt
+cd "$ROOT_DIR"/server
+php ./www/scripts/run-tests.sh --cache --php "$(which php)" --concurrency 4 --verbose --color --url http://server.local Hedley 2>&1 | tee /tmp/simpletest-result.txt
 egrep -i "([1-9]+ fail)|(Fatal error)|([1-9]+ exception)" /tmp/simpletest-result.txt && exit 1
 
 exit 0
