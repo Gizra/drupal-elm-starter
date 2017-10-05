19b274f5ffc080e4594038984dd319e790ad9913
Aron Novak <aron@gizra.com>
2017-10-05 17:04:45 +0200

    Revert "ci-scripts/docker_files/run_simpletest.sh: try without cache options too."
    This reverts commit ddef26cc6dfe35ead4210a2549e6ec43f31569d6.

--- ci-scripts/docker_files/run_simpletest.sh
+++ ci-scripts/docker_files/run_simpletest.sh
@@ -18,7 +18,7 @@ drush @server.local en simpletest -y
 cd "$ROOT_DIR"/server
 print_message "Starting SimpleTest tests"
 date
-php ./www/scripts/run-tests.sh --php "$(which php)" --concurrency 4 --verbose --color --url http://server.local SimpleTest 2>&1 | tee /tmp/simpletest-result.txt
+php ./www/scripts/run-tests.sh --cache --cache-modules --php "$(which php)" --concurrency 4 --verbose --color --url http://server.local SimpleTest 2>&1 | tee /tmp/simpletest-result.txt
 print_message "Finished SimpleTest tests"
 date
 grep -E -i "([1-9]+ fail)|(Fatal error)|([1-9]+ exception)" /tmp/simpletest-result.txt && exit 1
