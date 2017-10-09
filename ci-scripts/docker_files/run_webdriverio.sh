82949dc87e825d7c32c4cfc465144c9d98d00ccd
Aron Novak <aron@gizra.com>
2017-10-09 10:34:13 +0200

    ci-scripts/docker_files/run_webdriverio.sh: fix shellcheck, drop egrep, use grep -E.

--- ci-scripts/docker_files/run_webdriverio.sh
+++ ci-scripts/docker_files/run_webdriverio.sh
@@ -27,7 +27,7 @@ for SPEC in test/specs/*js; do
   for i in $(seq 3); do
     ./node_modules/.bin/wdio "$WDIO_CONF" --spec "$SPEC"  2>&1 | tee /tmp/"$SPEC_BASENAME"-"$i".txt
     WDIO_CMD_RET=$?
-    if egrep -i "(Debug errors appears, due to an error)" /tmp/"$SPEC_BASENAME"-"$i".txt; then
+    if grep -E -i "(Debug errors appears, due to an error)" /tmp/"$SPEC_BASENAME"-"$i".txt; then
       WDIO_RET=1
     else
       WDIO_RET=$WDIO_CMD_RET
