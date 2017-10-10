24f51610e35397d256fe3a2682dc723acbe07d0f
Aron Novak <aron@gizra.com>
2017-10-10 15:52:58 +0200

    ci-scripts/docker_files/run_simpletest.sh: revert to Hedley group.

--- ci-scripts/docker_files/run_simpletest.sh
+++ ci-scripts/docker_files/run_simpletest.sh
@@ -16,7 +16,7 @@ print_message "Run SimpleTest."
 export PATH="$HOME/.composer/vendor/bin:$PATH"
 drush @server.local en simpletest -y
 cd "$ROOT_DIR"/server
-php ./www/scripts/run-tests.sh --cache --cache-modules --php "$(which php)" --concurrency 4 --verbose --color --url http://server.local SimpleTest 2>&1 | tee /tmp/simpletest-result.txt
+php ./www/scripts/run-tests.sh --cache --cache-modules --php "$(which php)" --concurrency 4 --verbose --color --url http://server.local Hedley 2>&1 | tee /tmp/simpletest-result.txt
 grep -E -i "([1-9]+ fail)|(Fatal error)|([1-9]+ exception)" /tmp/simpletest-result.txt && exit 1
 
 exit 0
