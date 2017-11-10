d826b1b3bc6ce29584eb4a5b75e901261f1f0fc3
Aron Novak <aron@gizra.com>
2017-11-10 16:22:30 +0100

    fix simpletest invocation

--- ci-scripts/test_simpletest.sh
+++ ci-scripts/test_simpletest.sh
@@ -10,16 +10,17 @@ if [ -z "${BUILD_SERVER+x}" ] || [ "$BUILD_SERVER" -ne 1 ]; then
 fi
 
 # Load helper functionality.
-source helper_functions.sh
+source ci-scripts/helper_functions.sh
 
 # -------------------------------------------------- #
 # Run SimpleTest
 # -------------------------------------------------- #
 print_message "Run SimpleTest."
 export PATH="$HOME/.composer/vendor/bin:$PATH"
-drush @server.local en simpletest -y
+cd "$ROOT_DIR"/server/www
+drush en simpletest -y
 cd "$ROOT_DIR"/server
-php ./www/scripts/run-tests.sh --cache --cache-modules --php "$(which php)" --concurrency 4 --verbose --color --url http://server.local Hedley 2>&1 | tee /tmp/simpletest-result.txt
+php ./www/scripts/run-tests.sh --cache --cache-modules --php "$(which php)" --concurrency 4 --verbose --color --url http://127.0.0.1:8080 Hedley 2>&1 | tee /tmp/simpletest-result.txt
 grep -E -i "([1-9]+ fail)|(Fatal error)|([1-9]+ exception)" /tmp/simpletest-result.txt && exit 1
 
 exit 0
