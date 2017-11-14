896e99a4ac19f56a47225386c942a8112226e959
Aron Novak <aron@gizra.com>
2017-11-14 16:39:54 +0100

    fix simpletest invocation, navigate to Drupal docroot

--- ci-scripts/test_simpletest.sh
+++ ci-scripts/test_simpletest.sh
@@ -19,8 +19,8 @@ print_message "Run SimpleTest."
 export PATH="$HOME/.composer/vendor/bin:$PATH"
 cd "$ROOT_DIR"/server/www
 drush en simpletest -y
-cd "$ROOT_DIR"/server
-php ./www/scripts/run-tests.sh --cache --cache-modules --php "$(which php)" --concurrency 4 --verbose --color --url http://127.0.0.1:8080 Hedley 2>&1 | tee /tmp/simpletest-result.txt
+cd "$ROOT_DIR"/server/www
+php ./scripts/run-tests.sh --cache --cache-modules --php "$(which php)" --concurrency 4 --verbose --color --url http://127.0.0.1:8080 Hedley 2>&1 | tee /tmp/simpletest-result.txt
 grep -E -i "([1-9]+ fail)|(Fatal error)|([1-9]+ exception)" /tmp/simpletest-result.txt && exit 1
 
 exit 0
