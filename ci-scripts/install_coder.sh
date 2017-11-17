d5439e9f7dd63f9b19e00a8cbeb851cb81ea8770
Aron Novak <aron@gizra.com>
2017-11-17 14:35:19 +0100

    Revert "try to fix coder invocation by clearing composer cache [skip ci]"
    This reverts commit 1d062db757fd9aeaab13a39d87f4ecb53000020b.

--- ci-scripts/install_coder.sh
+++ ci-scripts/install_coder.sh
@@ -13,6 +13,5 @@ if [ -z "${CODE_REVIEW+x}" ] || [ "$CODE_REVIEW" -ne 1 ]; then
 fi
 
 cd "$TRAVIS_BUILD_DIR"
-composer clearcache
 composer global require drupal/coder
 phpcs --config-set installed_paths ~/.config/composer/vendor/drupal/coder/coder_sniffer
