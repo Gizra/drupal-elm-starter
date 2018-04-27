f3b35de4e033d481a9cfc8cb04ddb72077e7689b
Aron Novak <aron@gizra.com>
2018-04-27 15:45:22 +0200

    fix the passing of the base URL

--- ci-scripts/test_gatling.sh
+++ ci-scripts/test_gatling.sh
@@ -8,7 +8,7 @@ if [ -z "${BUILD_GATLING+x}" ] || [ "$BUILD_GATLING" -ne 1 ]; then
  exit 0;
 fi
 
-cd "$ROOT_DIR"/server/stress-test
+cd "$ROOT_DIR"/server/stress-test || exit 1
 bash run.sh
-SERVER_BASE_URL=http://server.local:8080
+export SERVER_BASE_URL=http://server.local:8080
 php acceptance.php gatling-charts-highcharts-bundle-2.3.0/results/server-*/js/global_stats.json
