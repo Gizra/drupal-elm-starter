dc9a47ca89bb42f00edd846c215bb19f70f77a8a
Aron Novak <aron@gizra.com>
2018-04-27 15:53:47 +0200

    ci-scripts/test_gatling.sh: fix base URL

--- ci-scripts/test_gatling.sh
+++ ci-scripts/test_gatling.sh
@@ -9,6 +9,6 @@ if [ -z "${BUILD_GATLING+x}" ] || [ "$BUILD_GATLING" -ne 1 ]; then
 fi
 
 cd "$ROOT_DIR"/server/stress-test || exit 1
-bash run.sh
 export SERVER_BASE_URL=http://server.local:8080
+bash run.sh
 php acceptance.php gatling-charts-highcharts-bundle-2.3.0/results/server-*/js/global_stats.json
