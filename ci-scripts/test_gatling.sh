6e33a9f3d3b11c1d4b1cc35e6e9d97124690dcef
Aron Novak <aron@gizra.com>
2018-05-18 09:22:33 +0200

    try to fix stress test execution by supplying a matching base URL

--- ci-scripts/test_gatling.sh
+++ ci-scripts/test_gatling.sh
@@ -9,13 +9,6 @@ if [ -z "${BUILD_GATLING+x}" ] || [ "$BUILD_GATLING" -ne 1 ]; then
 fi
 
 cd "$ROOT_DIR"/server/stress-test || exit 1
-export SERVER_BASE_URL=http://server.local:8080
+export SERVER_BASE_URL=http://127.0.0.1:8080
 bash run.sh
 php acceptance.php gatling-charts-highcharts-bundle-2.3.0/results/server-*/js/global_stats.json
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip ngrok-stable-linux-amd64.zip
-./ngrok http 8080  --log stdout --log-level debug &
-for i in `seq 600`; do
-  sleep 10
-  echo "."
-done;
