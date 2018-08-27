4419ce02e9545dc0dd379b3036dcd23c23ea6ac7
nedSaf <nader@gizra.com>
2018-08-27 13:41:45 +0300

    FIx checking wdio env logic

--- ci-scripts/install_webdriverio.sh
+++ ci-scripts/install_webdriverio.sh
@@ -4,26 +4,28 @@
 source ci-scripts/helper_functions.sh
 
 # Check the current build.
-if [ -z "${BUILD_WEBDRIVERIO+x}" ] || [ "$BUILD_WEBDRIVERIO" -ne 1 ] && ["$ENV" != 'frontend']; then
+if [ -z "${BUILD_WEBDRIVERIO+x}" ] || [ "$BUILD_WEBDRIVERIO" -ne 1 ]; then
  exit 0;
 fi
 
-# Install global packages.
-npm install -g elm@~0.18.0
-npm install -g bower@~1.8.2
-npm install -g gulp@~3.9.1
+if ["$ENV" == 'frontend']; then
+  # Install global packages.
+  npm install -g elm@~0.18.0
+  npm install -g bower@~1.8.2
+  npm install -g gulp@~3.9.1
 
-cd "$ROOT_DIR"/client || exit 1
-npm install
-bower install --allow-root
+  cd "$ROOT_DIR"/client || exit 1
+  npm install
+  bower install --allow-root
 
-elm-package install -y
-cp "$ROOT_DIR"/ci-scripts/LocalConfig.elm src/elm/LocalConfig.elm
+  elm-package install -y
+  cp "$ROOT_DIR"/ci-scripts/LocalConfig.elm src/elm/LocalConfig.elm
 
-# Run gulp in the background.
-gulp &
-# But wait for the availability of the app.
-until (curl --output /dev/null --silent --head --fail http://localhost:3000); do sleep 1; done
+  # Run gulp in the background.
+  gulp &
+  # But wait for the availability of the app.
+  until (curl --output /dev/null --silent --head --fail http://localhost:3000); do sleep 1; done
+fi
 
 # Install WDIO.
 cd "$ROOT_DIR"/wdio || exit 1
