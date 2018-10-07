85ac7c04bdbd17044b5b01033b4419c318b19d07
nedSaf <nader@gizra.com>
2018-10-07 16:55:15 +0300

    Fix travis config path

--- ci-scripts/test_webdriverio_backend.sh
+++ ci-scripts/test_webdriverio_backend.sh
@@ -30,7 +30,7 @@ for SPEC in ./specs/backend/*js; do
   SPEC_BASENAME=$(echo "$SPEC" | cut -d '/' -f 3 | cut -d '.' -f 1)
   sed "s/<<SPECNAME>>/$SPEC_BASENAME/" < ./travis-conf/$WDIO_CONF.orig > ./travis-conf/"$WDIO_CONF"
   for i in $(seq 3); do
-    ./node_modules/.bin/wdio "$WDIO_CONF" --spec "$SPEC"  2>&1 | tee /tmp/"$SPEC_BASENAME"-"$i".txt
+    ./node_modules/.bin/wdio ./travis-conf/"$WDIO_CONF" --spec "$SPEC"  2>&1 | tee /tmp/"$SPEC_BASENAME"-"$i".txt
     WDIO_CMD_RET=$?
     if grep -E -i "(Debug errors appears, due to an error)" /tmp/"$SPEC_BASENAME"-"$i".txt; then
       WDIO_RET=1
