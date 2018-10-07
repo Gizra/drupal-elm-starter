329c624bbf4da45d28f5ab6cd6ada1e05878dc62
nedSaf <nader@gizra.com>
2018-10-07 15:45:56 +0300

    update the test client script

--- ci-scripts/install_webdriverio.sh
+++ ci-scripts/install_webdriverio.sh
@@ -8,6 +8,10 @@ npm install -g elm@~0.18.0
 npm install -g bower@~1.8.2
 npm install -g gulp@~3.9.1
 
+# Install WDIO.
+cd "$ROOT_DIR"/wdio || exit 1
+npm install
+
 cd "$ROOT_DIR"/client || exit 1
 npm install
 bower install --allow-root
@@ -18,8 +22,9 @@ cp "$ROOT_DIR"/ci-scripts/LocalConfig.elm src/elm/LocalConfig.elm
 # Run gulp in the background.
 gulp &
 # But wait for the availability of the app.
-until (curl --output /dev/null --silent --head --fail http://localhost:3000); do sleep 1; done
-
-# Install WDIO.
-cd "$ROOT_DIR"/wdio || exit 1
-npm install
+c=0
+until (curl --output /dev/null --silent --head --fail http://localhost:3000); do
+  ((c++)) && ((c==180)) && exit 1
+  sleep 1
+done
+print_message "The webserver on port 3000 became available"
