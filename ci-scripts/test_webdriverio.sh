bd3a61fadac18c18b10460989ebceebf6879897d
nedSaf <nader@gizra.com>
2018-12-13 10:40:06 +0200

    Update cache for node modules

--- ci-scripts/test_webdriverio.sh
+++ ci-scripts/test_webdriverio.sh
@@ -52,7 +52,7 @@ done
 
 # If WDIO failed, check Watchdog and check if we can access the backend.
 if [[ $WDIO_ALL_RET -ne 0 ]]; then
-  print_error_message "There are at least one failing specs. See debug details and list below"
+  print_error_message "There is at least one failing spec. See debug details and list below"
   cd "$ROOT_DIR"/server/www
   export PATH="$HOME/.composer/vendor/bin:$PATH"
   drush cc drush
@@ -64,5 +64,46 @@ if [[ $WDIO_ALL_RET -ne 0 ]]; then
     print_error_message "$SPEC"
   done;
 
+  source "$ROOT_DIR"/server/travis.config.sh
+
+  cd /tmp
+  ! rm gdrive-linux-x64.zip
+  wget https://github.com/prasmussen/gdrive/files/879060/gdrive-linux-x64.zip
+  unzip gdrive-linux-x64.zip
+  chmod +x ./gdrive
+  mkdir ~/.gdrive
+  cp "$ROOT_DIR"/gdrive-service-account.json ~/.gdrive/
+  export PATH="$HOME/.composer/vendor/bin:$PATH"
+  GH_COMMENT=/tmp/db_urls
+  echo -n '{
+  "body": "' >> $GH_COMMENT
+
+    cd "$ROOT_DIR/server/www"
+    DB_FILE="drupal.sql"
+    drush sql-dump > "$DB_FILE"
+    gzip "$DB_FILE"
+    FAILED_SPEC=$(</tmp/test_results/failed_tests)
+    print_message "Uploading $DB_FILE, it contains a failed test"
+    ID=$(/tmp/gdrive upload --service-account gdrive-service-account.json "$DB_FILE.gz" | tail -n1 | cut -d ' ' -f 2)
+    /tmp/gdrive share --service-account gdrive-service-account.json "$ID"
+    URL=$(/tmp/gdrive info --service-account gdrive-service-account.json "$ID" | grep ViewUrl | sed s/ViewUrl:\ //)
+    echo -n "* DB dump after the failed WebdriverIO test [$FAILED_SPEC]($URL)." | tee -a $GH_COMMENT
+    echo ""
+    printf '\\n' >> $GH_COMMENT
+
+  echo '"}' >> $GH_COMMENT
+
+  # Todo: make it non-Gizra specific by detecting the user of the repository.
+  PR_URL=$(curl -H "Authorization: token $GH_TOKEN" -s  https://api.github.com/repos/"$GH_REPO"/pulls?head=Gizra:"${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}" | grep '"url"' | head -n1  | cut -d'"' -f 4)
+  PR_URL=${PR_URL/\/pulls\//\/issues\/}
+  if [[ -z "${PR_URL// }" ]]; then
+    print_error_message "Failed to detect related GitHub issue"
+  else
+    print_message "Detected issue: $PR_URL. Posting GitHub comment..."
+    ! curl -H "Authorization: token $GH_TOKEN" --data @$GH_COMMENT "$PR_URL"/comments
+  fi
 fi
+
+source "$ROOT_DIR/ci-scripts/post_cache.sh"
+
 exit $WDIO_ALL_RET
