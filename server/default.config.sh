daab531add02f5b37f747f23cee01aed04b9e72d
Aron Novak <aron@gizra.com>
2019-07-13 13:30:49 +0200

    default config

---
+++ server/default.config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#########################################################################################
+# YOU DO NOT NEED TO TAKE CARE OF THIS FILE IN CASE OF DDEV-BASED INSTALLATION.
+#
+# Configuration used for the native installation.
+#
+# Copy this file in the same directory, the filename of the copy should be "config.sh".
+#
+#########################################################################################
+
+
+# Modify the URL below to match your local domain the site will be accessible on.
+BASE_DOMAIN_URL="drupal-elm-starter.local"
+
+# Modify the MySQL settings below so they will match your own.
+MYSQL_USERNAME="root"
+MYSQL_PASSWORD=""
+MYSQL_HOSTNAME="localhost"
+MYSQL_DB_NAME="drupal_elm_starter"
