8962426368dfb36a79d76f892cc4eb931b5c1222
Aron Novak <aron@gizra.com>
2019-10-16 23:06:18 +0200

    travis stack update

--- ci-scripts/install_server.sh
+++ ci-scripts/install_server.sh
@@ -9,11 +9,14 @@ source ci-scripts/helper_functions.sh
 # -------------------------------------------------- #
 print_message "Install Docker Compose."
 sudo rm /usr/local/bin/docker-compose
-curl -s -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" > docker-compose
+curl -s -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" > docker-compose
 chmod +x docker-compose
 sudo mv docker-compose /usr/local/bin
 
 print_message "Upgrade Docker."
+sudo apt-get -y remove docker docker-engine docker.io containerd runc || true
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt -q update -y || true
 sudo apt -q install --only-upgrade docker-ce -y
 
@@ -36,5 +39,5 @@ cp "$ROOT_DIR/ci-scripts/global_config.yaml" ~/.ddev/
 print_message "Install Drupal."
 
 cd "$ROOT_DIR"/server || exit 1
-./install -y
+./install -y || ddev logs
 check_last_command
--- ci-scripts/install_webdriverio.sh
+++ ci-scripts/install_webdriverio.sh
@@ -3,11 +3,18 @@
 # Load helper functionality.
 source ci-scripts/helper_functions.sh
 
+# Install Google Chrome.
+sudo apt-get -qq update -q
+sudo apt-get -qq install dpkg -y -q
+wget -nv https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
 # Install Chromedriver.
-wget -nv https://chromedriver.storage.googleapis.com/74.0.3729.6/chromedriver_linux64.zip
+LATEST_CHROMEDRIVER=$(wget -q -O - http://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+wget -nv "http://chromedriver.storage.googleapis.com/$LATEST_CHROMEDRIVER/chromedriver_linux64.zip"
 unzip chromedriver_linux64.zip
 nohup ./chromedriver &
 
 # Install WDIO.
 cd "$ROOT_DIR"/wdio || exit 1
-npm install
+npm install > ./_build.log 2>&1 || ( EC=$?; cat ./_build.log; exit $EC )
